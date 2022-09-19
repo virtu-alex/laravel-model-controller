8faf0244b029fbc8ef684ddf500e1a54a54a03c2
virtu-alex <alessio.cordari@gmail.com>
2022-09-19 15:39:14 +0200

    feat: Add cards

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -5,21 +5,24 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <title>Document</title>
 </head>
 
 <body>
     <div class="container mt-5">
-        <div class="row">
+        <div class="row justify-content-center">
             @foreach ($movies as $movie)
                 <div class="col-4">
-                    <ul>
-                        <li> Image: <img src="{{ $movie->cover }}" alt="{{ $movie->title }}">
-                        <li> Original Title: {{ $movie->original_title }}</li>
-                        <li> Date: {{ $movie->date }}</li>
-                        <li> Vote: {{ $movie->vote }}</li>
-                    </ul>
+                    <div class="card bg-dark text-white m-3">
+                        <ul>
+                            <li> Original Title: {{ $movie->original_title }}</li>
+                            <li> Title: {{ $movie->title }}</li>
+                            <li> Nationality: {{ $movie->nationality }}</li>
+                            <li> Date: {{ $movie->date }}</li>
+                            <li> Vote: {{ $movie->vote }}</li>
+                        </ul>
+                    </div>
                 </div>
             @endforeach
         </div>
